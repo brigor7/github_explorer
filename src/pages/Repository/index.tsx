3a2cb530c7631a08e18f4c7dd4e2906ae62fdb9c
=Bruno Igor <=brigor7@gmail.com>
2020-08-27 17:36:02 -0300

    styles Header from  <Repository>

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import logoImg from '../../assets/github-logo.svg';
+import { Header } from './styles';
+import { FiChevronsLeft } from 'react-icons/fi';
 
 interface RepositoryParams {
   repository: string;
@@ -7,6 +10,16 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+    </>
+  );
 };
 export default Repository;
