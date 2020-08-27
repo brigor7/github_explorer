e42e15d52a732f7dd27364a4667bbf1d620e2365
=Bruno Igor <=brigor7@gmail.com>
2020-08-27 18:41:08 -0300

    repository page construction

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import logoImg from '../../assets/github-logo.svg';
-import { Header } from './styles';
-import { FiChevronsLeft } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
 
 interface RepositoryParams {
   repository: string;
@@ -19,6 +19,47 @@ const Repository: React.FC = () => {
           Voltar
         </Link>
       </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://unipublicabrasil.com.br/curso-gratuito/views/assets/img/avatar/homem-5.png"
+            alt="Avatar"
+          />
+          <div>
+            <strong>brigor7/match_renascer</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+              cum qui consequatur ex, eveniet assumenda?
+            </p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Star</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues abertos</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>Lorem ipsum dolor sit amet.</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+              nihil voluptatem odio, assumenda at quo.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   );
 };
--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -21,3 +21,96 @@ export const Header = styled.header`
     }
   }
 `;
+
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
+  header {
+    display: flex;
+    align-items: center;
+    img {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+    }
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #3d3d3d;
+        font-weight: bold;
+      }
+      p {
+        font-size: 14px;
+        color: #737373;
+        margin-top: 3px;
+      }
+    }
+  }
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      & + li {
+        margin-left: 80px;
+      }
+    }
+
+    strong {
+      display: block;
+      font-size: 36px;
+      color: #3d3d3d;
+    }
+
+    span {
+      display: block;
+      margin-top: 3px;
+      color: #6c6c6c;
+    }
+  }
+`;
+
+export const Issues = styled.section`
+  margin-top: 80px;
+
+  a {
+    background: #fff;
+    border-radius: 10px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.5s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    div {
+      margin-left: 16px;
+      strong {
+        color: #3d3d3d;
+        font-size: 20px;
+      }
+      p {
+        font-size: 18px;
+        color: #8a8a8a;
+        margin-top: 5px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
